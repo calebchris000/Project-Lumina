84f8a16a53700cde6d0e2ff035647445f8182e78
Caleb Nwaizu <calebchris000@gmail.com>
2023-09-28 13:30:58 +0100

    Add parent, student, and contact  model to base

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,2 +1,4 @@
-from src.app.school_services.teacher.models.teacher import Teacher
+# from src.app.school_services.teacher.models.teacher import Teacher
 from src.app.school_services.credentials.models.credentials import Credential
+from src.app.school_services.student.models.student import Student
+from src.app.school_services.contact.models.contact import Contact
--- src/core/routers/api/v1/routers.py
+++ src/core/routers/api/v1/routers.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter
 from src.app.school_services.teacher.v1.routers.teacher import teacher_router
 from src.app.auth.v1.routers.signup import signup_router
 from src.app.auth.v1.routers.login import login_router
+from src.app.school_services.credentials.v1.routers.credential import credential_router
+from src.app.school_services.student.v1.routers.student import student_router
+
 router = APIRouter()
 
 
-
 router.include_router(teacher_router)
+router.include_router(student_router)
 router.include_router(signup_router)
 router.include_router(login_router)
+router.include_router(credential_router)
