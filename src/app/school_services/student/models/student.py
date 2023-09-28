adbc99866acebf35d620f61273a9a5d87a03edc2
Caleb Nwaizu <calebchris000@gmail.com>
2023-09-28 14:15:46 +0100

    Add foreign key to class

--- src/app/school_services/student/models/student.py
+++ src/app/school_services/student/models/student.py
@@ -5,7 +5,7 @@ from tortoise import fields
 
 class Student(User):
     parent_id = fields.UUIDField(null=False)
-    enrolled_class = fields.UUIDField()
+    enrolled_class = fields.ForeignKeyField('models.Course', related_name='course')
     school_email = fields.CharField(max_length=30,null=True)
     contact = fields.ManyToManyField('models.Contact', related_name='contacts')
     
