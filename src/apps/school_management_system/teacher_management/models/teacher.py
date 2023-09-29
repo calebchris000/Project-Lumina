8a1b207cf29f5f4359597490ead5f6375a3bdcc4
Caleb Nwaizu <calebchris000@gmail.com>
2023-09-29 20:37:33 +0100

    Add user and school contact

--- src/apps/school_management_system/teacher_management/models/teacher.py
+++ src/apps/school_management_system/teacher_management/models/teacher.py
@@ -7,9 +7,9 @@ from tortoise import fields
 class Teacher(User):
     subject_taught = fields.ForeignKeyField('models.Course', related_name='subject')
     qualifications = fields.CharEnumField(QUALIFICATION, default=QUALIFICATION.CLEAR_BACKGROUND_CHECK, null=False)
-    contact = fields.ManyToManyField('models.Contact', related_name='contact')
+    user_contact = fields.ManyToManyField('models.UserContact', related_name='user_contact')
+    school_contact = fields.OneToOneField('models.SchoolContact', related_name='school_contact')
     years_of_experience = fields.DecimalField(max_digits=3, decimal_places=1)
     supervisor = fields.CharField(max_length=20)
     attendance_records = fields.CharField(max_length=20, default=1)
     teaching_schedule = fields.CharField(max_length=20)
-    teacher_id = fields.IntField(default=generate_user_id())
