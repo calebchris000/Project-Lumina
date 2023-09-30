f861b3b2ccf55ddb5973a4c3dcc37ca1145e0974
Caleb Nwaizu <calebchris000@gmail.com>
2023-09-30 15:56:15 +0100

    Adjust numbers to 8

--- src/apps/shared/generate_user_id.py
+++ src/apps/shared/generate_user_id.py
@@ -2,5 +2,5 @@ import random
 
 
 def generate_user_id():
-    number = "".join(str(random.randint(0, 9)) for _ in range(10))
+    number = "".join(str(random.randint(0, 9)) for _ in range(8))
     return int(number)
