28d83e49dae4cfe7befe33a4f2ca4fc8ecffb9db
Caleb Nwaizu <calebchris000@gmail.com>
2023-09-30 15:58:44 +0100

    Add student attendant router

--- src/core/routers/api/v1/routers.py
+++ src/core/routers/api/v1/routers.py
@@ -3,10 +3,12 @@ from src.apps.school_management_system.teacher_management.routers.teacher import
 from src.apps.authentication.v1.routers.signup import signup_router
 from src.apps.authentication.v1.routers.login import login_router
 from src.apps.school_management_system.student_management.routers.student import student_router
-router = APIRouter()
+from src.apps.school_management_system.student_management.routers.student_attendance import student_attendance_router
 
+router = APIRouter()
 
 router.include_router(teacher_router)
 router.include_router(student_router)
+router.include_router(student_attendance_router)
 router.include_router(signup_router)
 router.include_router(login_router)
