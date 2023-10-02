b45382da616d6116987a91a14cbdaa9a95d2dc94
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-02 18:48:03 +0100

    Adjust subject_course

--- src/apps/school_management_system/course_subject/models/course.py
+++ src/apps/school_management_system/course_subject/models/course.py
@@ -5,6 +5,5 @@ from tortoise import fields
 
 class Course(BaseModel):
     name = fields.CharField(max_length=30, unique=True)
-    course_id = fields.IntField(max_length=8, index=True)
     description = fields.TextField()
     
--- src/apps/school_management_system/course_subject/models/subject.py
+++ src/apps/school_management_system/course_subject/models/subject.py
@@ -6,5 +6,5 @@ from tortoise import fields
 class Subject(BaseModel):
     name = fields.CharField(max_length=30, unique=True)
     course = fields.ForeignKeyField('models.Course', related_name='course')
+    teacher = fields.ForeignKeyField('models.Teacher', to_fields='teacher_id', null=True, related_name='subjects')
     description = fields.TextField()
-    
