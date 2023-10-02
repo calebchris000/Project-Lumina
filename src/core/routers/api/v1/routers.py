447f74877e3c5214b010ff01897545bfe2b890a2
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-02 19:28:53 +0100

    Add teacher_attendance router

--- src/core/routers/api/v1/routers.py
+++ src/core/routers/api/v1/routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from src.apps.school_management_system.teacher_management.routers.teacher import teacher_router
+from src.apps.school_management_system.teacher_management.routers.teacher_attendance import teacher_attendance_router
 from src.apps.school_management_system.teacher_management.routers.teacher_qualification import teacher_qualification_router
 from src.apps.authentication.v1.routers.signup import signup_router
 from src.apps.authentication.v1.routers.login import login_router
@@ -16,6 +17,7 @@ router.include_router(teacher_router)
 router.include_router(student_router)
 router.include_router(student_attendance_router)
 router.include_router(teacher_qualification_router)
+router.include_router(teacher_attendance_router)
 router.include_router(student_grade_router)
 router.include_router(student_user_contact)
 router.include_router(course_router)
