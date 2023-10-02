5aaad462ef6cd7b2879f96c091e6ec546aeae11f
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-02 19:30:43 +0100

    remove attendance_record since dedicated table exists

--- src/apps/school_management_system/teacher_management/models/teacher.py
+++ src/apps/school_management_system/teacher_management/models/teacher.py
@@ -7,4 +7,3 @@ from tortoise import fields
 class Teacher(User):
     teacher_id = fields.IntField(unique=True)
     years_of_experience = fields.DecimalField(max_digits=3, decimal_places=1)
-    attendance_records = fields.CharField(max_length=20, default=1)
