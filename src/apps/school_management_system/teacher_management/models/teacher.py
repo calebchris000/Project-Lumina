429b22008c13ea06916acddf02e9afa236b195e5
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-04 19:57:54 +0100

    Add class field to teacher

--- src/apps/school_management_system/teacher_management/models/teacher.py
+++ src/apps/school_management_system/teacher_management/models/teacher.py
@@ -7,3 +7,4 @@ from tortoise import fields
 class Teacher(User):
     teacher_id = fields.IntField(unique=True)
     years_of_experience = fields.DecimalField(max_digits=3, decimal_places=1)
+    school_class = fields.ForeignKeyField('models.SchoolClass', null=True, related_name='teacher_class')
