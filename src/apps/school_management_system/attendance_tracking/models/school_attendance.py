e34472f75eaf7fbeedf81aa639e6d34171810f69
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-05 08:53:57 +0100

    more resolutions

--- src/apps/school_management_system/attendance_tracking/models/school_attendance.py
+++ src/apps/school_management_system/attendance_tracking/models/school_attendance.py
@@ -5,7 +5,8 @@ from src.database.model import BaseModel
 from tortoise import fields
 
 class SchoolAttendance(BaseModel):
-    student = fields.ForeignKeyField('models.Student', related_name='school_attendances')
+    student = fields.ForeignKeyField('models.Student', related_name='school_attendances', null=True)
+    teacher = fields.ForeignKeyField('models.Teacher', related_name='school_attendances', null=True)
     present = fields.BooleanField(default=False)
     reason = fields.TextField(null=True)
     
