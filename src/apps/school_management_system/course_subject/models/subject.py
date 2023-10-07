97ac081003841eafe1f2bbb0ba1b840ded8e49b9
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-07 07:19:15 +0100

    Add from and to date

--- src/apps/school_management_system/course_subject/models/subject.py
+++ src/apps/school_management_system/course_subject/models/subject.py
@@ -6,9 +6,9 @@ from tortoise import fields
 class Subject(BaseModel):
     name = fields.CharField(max_length=30, unique=True)
     teacher = fields.ForeignKeyField('models.Teacher', related_name='teacher_subjects', null=True)
-    from_date = fields.DatetimeField()
-    to_date = fields.DatetimeField()
-    description = fields.TextField()
+    from_date = fields.DatetimeField(null=True)
+    to_date = fields.DatetimeField(null=True)
+    description = fields.TextField(null=True)
     
     class Meta:
         table = 'subjects'
