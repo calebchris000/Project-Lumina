737cb29a7d659730e50943bd41be1d8160ef0217
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-22 15:03:39 +0100

    Modify code to make it flexible by customising week dates

--- src/shared/date/get_previous_week_dates.py
+++ src/shared/date/get_previous_week_dates.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta, date
 def get_previous_week_dates(year = datetime.now().year, week_number=1):
     specified_date = datetime.fromisocalendar(year, week_number, 1)
     
-    monday_to_friday_dates = []
     monday = specified_date
     friday = monday + timedelta(days=4)
     
